abf28be9038b1d0307f596aed88f6b80bc302b19
Artem A. Razin <Artem.Razin@gmail.com>
2025-03-03 12:59:30 +0300

    Initial files.

---
+++ AvaloniaCustomControlStyling/Controls/CustomControl.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Media;
+
+namespace AvaloniaCustomControlStyling.Controls
+{
+    internal class CustomControl : Control
+    {
+        public static readonly StyledProperty<Color> BackgroundColorProperty =
+            AvaloniaProperty.Register<CustomControl, Color>(nameof(BackgroundColor), Colors.Transparent);
+
+        public Color BackgroundColor
+        {
+            get => GetValue(BackgroundColorProperty);
+            set
+            {
+                SetValue(BackgroundColorProperty, value);
+                Debug.Assert(false);
+            }
+        }
+
+        public CustomControl()
+        {
+            this.GetObservable(BackgroundColorProperty)
+                .Subscribe(_ => InvalidateVisual());
+        }
+
+        public override void Render(DrawingContext context)
+        {
+            context.DrawRectangle(
+                new SolidColorBrush(BackgroundColor),
+                null,
+                Bounds);
+        }
+    }
+}
